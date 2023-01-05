19a8087f591945eef4d2c7db1742cb4a5c330b00
michaelkeates <mail@michaelkeates.co.uk>
2023-01-05 00:33:47 +0000

    Update fonts.js

--- components/fonts.js
+++ components/fonts.js
@@ -1,9 +1,6 @@
 const Fonts = () => (
-  //<style jsx global>{`
-  //  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap');
-  //`}</style>
   <style jsx global>{`
-  @import url('https://applesocial.s3.amazonaws.com/assets/styles/fonts/sanfrancisco/sanfranciscodisplay-regular-webfont.woff');
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap');
   `}</style>
 )
 export default Fonts
