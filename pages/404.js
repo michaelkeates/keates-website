67e8fa3a00d307aef4f6c360b331d729f2b1aba9
michaelkeates <mail@michaelkeates.co.uk>
2023-01-08 22:47:53 +0000

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -14,10 +14,21 @@ const NotFound = () => {
   return (
     <Container>
       <Section delay={0.1}>
-        <Heading as="h1" fontFamily="Roboto" fontWeight="">
-          Not found
-        </Heading>
-        <Text>The page you&apos;re looking for was not found.</Text>
+        <Box
+          borderRadius="lg"
+          mb={6}
+          p={3}
+          textAlign="center"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          css={{ backdropFilter: 'blur(10px)' }}
+          padding="15px;"
+        >
+          Sorry! 🤦‍♂️
+        </Box>
+
+        <Text align="center" paddingTop="7px">
+          The page you&apos;re looking for was not found.
+        </Text>
         <Divider my={6} />
         <Box my={6} align="center">
           <NextLink href="/" passHref>
