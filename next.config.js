57a35e0d5560992de4272ea618478dd05ddaaa60
michaelkeates <mail@michaelkeates.co.uk>
2023-01-28 16:42:21 +0000

    fixes and updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['blog.michaelkeates.co.uk','avatars.githubusercontent.com','repository-images.githubusercontent.com'],
+    domains: ['blog.michaelkeates.co.uk','avatars.githubusercontent.com','repository-images.githubusercontent.com', 'opengraph.githubassets.com'],
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
