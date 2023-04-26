d12c2bcb2a95b9b5eb1e11e64e1b3fc3dd519ef1
michaelkeates <mail@michaelkeates.co.uk>
2023-04-26 21:40:06 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ if (typeof window !== 'undefined') {
 function Website({ Component, pageProps, router }) {
   return (
     <Chakra cookies={pageProps.cookies}>
+      <Fonts />
       <Layout router={router}>
         <AnimatePresence
           mode="wait"
