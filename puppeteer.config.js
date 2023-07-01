6724de855f141a73bfdbb4d15730a67cacf06db3
michaelkeates <mail@michaelkeates.co.uk>
2023-07-01 14:58:53 +0100

    vercel cache fix

---
+++ puppeteer.config.js
@@ -0,0 +1,14 @@
+const os = require('os');
+const path = require('path');
+
+module.exports = {
+  launch: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      `--disk-cache-dir=${path.join(os.tmpdir(), '/vercel/.cache/puppeteer')}`, // Set the cache directory
+    ],
+  },
+  browserContext: 'default',
+};
