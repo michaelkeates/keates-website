2a2a2458443b4d194f689be85787d4d64ae3ef87
michaelkeates <mail@michaelkeates.co.uk>
2023-07-03 20:57:14 +0100

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -559,20 +559,6 @@ const Home = () => {
             >
               <DockerIcon />
             </Box>
-            <Box
-              borderRadius="lg"
-              mb={6}
-              p={3}
-              textAlign="center"
-              bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-              css={{ backdropFilter: 'blur(10px)' }}
-              padding="10px;"
-              href="Typescript"
-              title="Git"
-              boxShadow="0px 0px 12px 0px rgba(0,0,0,0.05);"
-            >
-              <GitIcon />
-            </Box>
           </SimpleGrid>
         </Section>
         <Divider />
@@ -613,6 +599,20 @@ const Home = () => {
               boxShadow="0px 0px 12px 0px rgba(0,0,0,0.05);"
             >
               <XamarinIcon />
+            </Box>
+            <Box
+              borderRadius="lg"
+              mb={6}
+              p={3}
+              textAlign="center"
+              bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+              css={{ backdropFilter: 'blur(10px)' }}
+              padding="10px;"
+              href="Typescript"
+              title="Git"
+              boxShadow="0px 0px 12px 0px rgba(0,0,0,0.05);"
+            >
+              <GitIcon />
             </Box>
           </SimpleGrid>
         </Section>
